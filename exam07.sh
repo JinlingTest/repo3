269f6acdc2efa0769bd31e5c46c221dad512ab66
Jin <70491992@qq.com>
2019-08-06 15:56:15 +0800

    atfernoon   clear log 3

--- exam07.sh
+++ exam07.sh
@@ -5,13 +5,13 @@
 
 if [ ${USER} != "root"  ]; then
 echo "该脚本只有root才可以执行";
-exit
+exit 100
 fi
 
 
 if [ ! -f /var/log/messages ];then
 echo "文件不存在"
-exit
+exit 101
 fi
 
 function clear_log
@@ -22,3 +22,8 @@ cat /var/log/message1000 > /var/log/messages
 }
 
 clear_log
+if [ $? -eq 0 ];then
+echo "the messages log is Clean!!"
+else
+echo "不知道为什么 这个没执行成功"
+fi
