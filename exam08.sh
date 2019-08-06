afaec5da2d3840af8be90f76b611cb7397e3c6d6
Jin <70491992@qq.com>
2019-08-06 16:25:38 +0800

    atfernoon   clear log 8[]

--- exam08.sh
+++ exam08.sh
@@ -3,7 +3,7 @@
 #
 
 read -p "请你输入该生的年龄：" num
-if [ ${num} -le 7 -o ${num} -ge 12 ];then
+if [[ ${num} -le 7 || ${num} -ge 12 ]];then
 echo "该生不可以入学"
 else
 echo "该生可以入学"
---
+++ exam09.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+#
+
+
+read -p "请你输入名字： " name
+if [ ${name} == '赵*' ];then
+echo "录取"
+else
+echo "不录取"
+fi
