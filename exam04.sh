509c847a7cb9c8c2bcd34a46e2f0b7f0dd6bc531
Jin <70491992@qq.com>
2019-08-06 10:29:06 +0800

    这是我今天编的第二段代码

--- exam04.sh
+++ exam04.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # if statement I
 
-if ls /etc &> /dev/null; then
-echo " the etc directory is OK"
+if ls /etca &> /dev/null; then
+echo " the  directory is OK"
+else
+echo " the  directory is error"
 fi
