907d62614560be053f15e8b69b5b68d3089cccd5
Jin <70491992@qq.com>
2019-08-09 17:24:33 +0800

    fun 1 2 3 4 5 6 7

--- exam20.sh
+++ exam20.sh
@@ -21,7 +21,31 @@ fun4()
 echo "this is fun4"
 return 100
 }
+
+fun5()
+{
+echo "this is fun5 Line 1"
+return 105
+echo "this is fun5 Line 2"
+}
+
+fun6()
+{
+echo "this is fun6 Line 1"
+exit 106
+echo "this is fun6 Line 2"
+}
+
+fun7()
+{
+echo "this is fun7 Line 1"
+}
+
+
 fun1
 fun2
 fun3 100 200
 fun4
+fun5
+fun6
+fun7
